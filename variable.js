8abf0c2a67843e0e935c63b841ae8321fd8a3320
narasimha4517 <narasimha79307@gmail.com>
2025-07-26 10:04:24 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,16 @@
+const vesion =20;
+function add(a,b){
+    return a+b;
+}
+const add1 =(a,b)=>a+b;
+const greet =()=>console.log("HELLO");
+const square =x=>x*x;
+const multiply=(a,b)=>{
+    let result=a*b;
+    return result;
+
+};
+
+greet();
+console.log(add(10,20));
+console.log(add1(5,3));
